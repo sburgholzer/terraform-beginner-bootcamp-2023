9553e70d532e707371663b49fe813f772d8d23d8
Scott Burgholzer <scott@weatherbyscott.com>
2023-09-26 01:23:40 +0000

    #27 enabled static website hosting, uploaded index and error files via terraform

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
+  value       = module.terrahouse_aws.bucket_name
+}
+
+output "s3_website_endpoint" {
+  description = "Endpoint for our S3 bucket"
+  value       = module.terrahouse_aws.website_endpoint
 }
