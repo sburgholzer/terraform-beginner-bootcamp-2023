7127024282db8b1156f0b5f838edbb7c8a379a95
Scott Burgholzer <scott@weatherbyscott.com>
2023-09-25 21:10:22 -0500

    #29 create CDN and server S3 bucket from CDN (#30)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -12,8 +12,8 @@ variable "bucket_name" {
   type        = string
 
   validation {
-    condition     = (
-      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
+    condition = (
+      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
       can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
     )
     error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
