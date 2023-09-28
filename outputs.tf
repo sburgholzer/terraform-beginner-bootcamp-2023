a534be146bb60a2cc11d3f6e7039e8749d8b987b
Scott Burgholzer <scott@weatherbyscott.com>
2023-09-27 20:49:07 -0500

    #33 invalidate cloudfront stack when content version changes (#34)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "s3_website_endpoint" {
   description = "Endpoint for our S3 bucket"
   value       = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
