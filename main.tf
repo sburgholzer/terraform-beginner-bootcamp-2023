3a6e6cc0a42f6c6f37f912b5f238cc2335f92433
Scott Burgholzer <scott@weatherbyscott.com>
2023-10-11 22:13:38 +0000

    #42 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   /*cloud {
     organization = "Scott-Private"
 
@@ -8,7 +14,13 @@ terraform {
   }*/
 }
 
-module "terrahouse_aws" {
+provider "terratowns" {
+    endpoint = "http://localhost:4567/api"
+    user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
+    token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+/*module "terrahouse_aws" {
   source              = "./modules/terrahouse_aws"
   user_uuid           = var.user_uuid
   bucket_name         = var.bucket_name
@@ -16,4 +28,5 @@ module "terrahouse_aws" {
   error_html_filepath = var.error_html_filepath
   content_version     = var.content_version
   assets_path         = var.assets_path
-}
+}*/
+
